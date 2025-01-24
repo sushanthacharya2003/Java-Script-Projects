3468cb7f2127b1bfb0085c2046b5e9bb90426ca2
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-01-24 15:52:52 +0530

    added js file

---
+++ price_range_slider/price.js
@@ -0,0 +1,8 @@
+const min_price = document.getElementById("minprice");
+const max_price = document.getElementById("maxprice");
+const priceRange = document.getElementById("price-range");
+
+function updatePriceRange() {
+  const min = parseInt(min_price.value);
+  const max = parseInt(max_price.value);
+}

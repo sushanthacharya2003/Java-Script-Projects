25e9c7faac34407585977380bf34d20459bcfa64
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-01-31 15:35:31 +0530

    first mini project complete

--- price_range_slider/price.js
+++ price_range_slider/price.js
@@ -5,4 +5,13 @@ const priceRange = document.getElementById("price-range");
 function updatePriceRange() {
   const min = parseInt(min_price.value);
   const max = parseInt(max_price.value);
+  if (min > max) {
+    min_price.value = max;
+  } else if (max < min) {
+    max_price.value = min;
+  }
+  priceRange.textContent = `${min_price.value}-${max_price.value}`;
+  min_price.addEventListener("input", updatePriceRange);
+  max_price.addEventListener("input", updatePriceRange);
+  updatePriceRange();
 }
